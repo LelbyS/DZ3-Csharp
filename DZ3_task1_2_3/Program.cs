cf5fd1a3f96b47faeae9f5cb9fb18b423ec1ce19
Sergei Dydin <dydin_serg@mail.ru>
2023-04-04 11:09:12 +0300

    add void Main

--- DZ3_task1_2_3/Program.cs
+++ DZ3_task1_2_3/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿Main();
+
+void Main()
+{
+    bool Working = true;
+    while (Working)
+    {
+        Console.WriteLine("Enter task number or exit");
+        string task_number = Console.ReadLine();
+        switch(task_number)
+        {
+            case "1": Task001();
+            break;
+
+            case "2": Task002();
+            break;
+
+            case "3": Task003();
+            break;
+
+            case "exit": Working = false;
+            break;
+
+            default: Console.WriteLine("Invalid input. Repeat");
+            break;
+        }
+        
+    }
+}
+
+void Task001()
+{
+
+}
+
+void Task002()
+{
+
+}
+
+void Task003()
+{
+
+}
+
