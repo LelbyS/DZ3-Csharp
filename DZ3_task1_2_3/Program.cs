1bce737cab1157f2fbb40e1fb0c9ef27008d7c15
Sergei Dydin <dydin_serg@mail.ru>
2023-04-04 12:16:39 +0300

    add solution Task002

--- DZ3_task1_2_3/Program.cs
+++ DZ3_task1_2_3/Program.cs
@@ -50,11 +50,22 @@ bool FiveDigit(int number)
     return false;
 }
 
+// Напишите программу которая на вход принимает координаты двух точек и находит их расстояние между ними в 3D пространстве
 void Task002()
 {
+    int x1 = IntRead("x1");
+    int y1 = IntRead("y1");
+    int z1 = IntRead("z1");
+    int x2 = IntRead("x2");
+    int y2 = IntRead("y2");
+    int z2 = IntRead("z2");
 
+    double result = Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2) + Math.Pow(z1 - z2,2));
+
+    Console.WriteLine($"Cut length: {result}");
 }
 
+// Напишите программу, которая принимает на вход число N и выдает таблицу кубов чисел от 1 до N
 void Task003()
 {
 
